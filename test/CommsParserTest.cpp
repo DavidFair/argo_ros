7159bb0127fc547e807a6145e522afa9e3c84a06
David Fairbrother <DavidFair@users.noreply.github.com>
2018-04-26 18:45:08 +0100

    Add checksum handling to ROS driver

--- test/CommsParserTest.cpp
+++ test/CommsParserTest.cpp
@@ -1,8 +1,27 @@
+#include <gmock/gmock.h>
 #include <string>
 
-#include <gtest/gtest.h>
-
 #include "CommsParser.hpp"
+
+using ::testing::HasSubstr;
+
+namespace {
+
+uint8_t getChecksumVal(const std::string &s) {
+  uint8_t sum = 0;
+  for (char c : s) {
+    sum += (int)c;
+  }
+  return sum;
+}
+
+std::string appendChecksumToString(std::string input) {
+  uint8_t checksumVal = getChecksumVal(input);
+  input.append(" chk:").append(std::to_string(checksumVal)).append("\n");
+  return input;
+}
+
+} // End of anonymous namespace
 
 TEST(CommsParserTest, correctCommandTypeIsDetermined) {
   // None
@@ -25,8 +44,10 @@ TEST(CommsParserTest, encoderIsParsed) {
   const int leftEncCount = 9876;
   const int rightEncCount = 8765;
 
-  const std::string input = "!e LEFT:" + std::to_string(leftEncCount) +
-                            " RIGHT:" + std::to_string(rightEncCount) + '\n';
+  std::string input = "!e LEFT:" + std::to_string(leftEncCount) +
+                      " RIGHT:" + std::to_string(rightEncCount);
+
+  input = appendChecksumToString(std::move(input));
 
   auto result = CommsParser::parseEncoderCommand(input);
   EXPECT_TRUE(result.isValid);
@@ -49,13 +70,30 @@ TEST(CommsParserTest, speedIsParsed) {
   const int leftSpeed = 1234;
   const int rightSpeed = 2345;
 
-  const std::string input = "!s LEFT:" + std::to_string(leftSpeed) +
-                            " RIGHT:" + std::to_string(rightSpeed) + '\n';
+  std::string input = "!s LEFT:" + std::to_string(leftSpeed) +
+                      " RIGHT:" + std::to_string(rightSpeed);
+
+  input = appendChecksumToString(std::move(input));
 
   auto result = CommsParser::parseSpeedCommand(input);
   EXPECT_TRUE(result.isValid);
   EXPECT_EQ(result.leftWheel, leftSpeed);
   EXPECT_EQ(result.rightWheel, rightSpeed);
+}
+
+TEST(CommsParserTest, pwmIsParsed) {
+  const int leftPwm = 123;
+  const int rightPwm = 234;
+
+  std::string input = "!m L_PWM:" + std::to_string(leftPwm) +
+                      " R_PWM:" + std::to_string(rightPwm);
+
+  input = appendChecksumToString(std::move(input));
+
+  auto result = CommsParser::parsePwmCommand(input);
+  EXPECT_TRUE(result.isValid);
+  EXPECT_EQ(result.leftWheel, leftPwm);
+  EXPECT_EQ(result.rightWheel, rightPwm);
 }
 
 TEST(CommsParserTest, mangledSpeedInputIsHandled) {
@@ -76,8 +114,24 @@ TEST(CommsCommands, getSpeedCommand) {
 
   const std::string expectedString{
       "!T L_SPEED:" + std::to_string(expectedLeftSpeed) +
-      " R_SPEED:" + std::to_string(expectedRightSpeed) + '\n'};
+      " R_SPEED:" + std::to_string(expectedRightSpeed) + " chk:"};
 
   auto result = CommsParser::getSpeedCommand(targetSpeed);
-  EXPECT_EQ(expectedString, result);
+  EXPECT_THAT(result, HasSubstr(expectedString));
+}
+
+TEST(CommsCommands, speedCommandUsesChecksum) {
+  const int expectedLeftSpeed = 1234;
+  const int expectedRightSpeed = 2345;
+  SpeedData targetSpeed{expectedLeftSpeed, expectedRightSpeed};
+
+  const std::string inputString{
+      "!T L_SPEED:" + std::to_string(expectedLeftSpeed) +
+      " R_SPEED:" + std::to_string(expectedRightSpeed)};
+  uint8_t checksumVal = getChecksumVal(inputString);
+
+  const std::string expectedChecksum = {" chk:" + std::to_string(checksumVal)};
+
+  auto result = CommsParser::getSpeedCommand(targetSpeed);
+  EXPECT_THAT(result, HasSubstr(expectedChecksum));
 }
